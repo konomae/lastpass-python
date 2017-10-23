b2adc927d30dc6666cdd3b156f7e28c9068a9645
konomae <konomae@users.noreply.github.com>
2017-10-23 16:45:30 +0900

    Print notes

--- example/example.py
+++ example/example.py
@@ -34,4 +34,4 @@ except LastPassIncorrectYubikeyPasswordError as e:
 
 
 for index, i in enumerate(vault.accounts):
-    print("{} {} {} {} {} {} {}".format(index + 1, i.id, i.name, i.username, i.password, i.url, i.group))
+    print("{} {} {} {} {} {} {} {}".format(index + 1, i.id, i.name, i.username, i.password, i.url, i.group, i.notes))
